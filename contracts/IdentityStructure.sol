59f59a563c49e412e5cea2d63e59b38dbd2f831b
STYJ <simon.tan.yu.jing@gmail.com>
2018-07-27 20:14:53 +0800

    updating files

---
+++ contracts/IdentityStructure.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.24;
+
+library IdentityStructure {
+    
+    struct Attribute {
+        
+    }
+        
+    struct Structure {
+        mapping(string => Attribute) attributes;
+        
+        
+    }
+    
+    
+    
+    
+}
---
+++ contracts/SelfSovereignIdentity.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.24;
+import "./IdentityStructure.sol";
+
+contract SelfSovereignIdentity{
+    
+    
+    // Variables
+    mapping(string => Structure) public idTypes;
+    
+    
+    
+    
+    
+    // Events
+    
+    
+    
+    
+    
+    // Modifiers
+    
+    
+    
+    
+    // Functions
+ 
+    constructor() public{
+
+    }
+    
+}
